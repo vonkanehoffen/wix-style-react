541ce1b043b64e285e65e8b739302d79c52cfc09
Gilad Segal <gilads@wix.com>
2019-04-29 15:18:28 +0300

    Add breadcrumbs types

--- src/Breadcrumbs/index.d.ts
+++ src/Breadcrumbs/index.d.ts
@@ -1,4 +1,27 @@
-export type BreadcrumbsProps = any;
+import * as React from 'react';
+import WixComponent, {WixComponentProps} from '../BaseComponents/WixComponent';
 
-declare const Breadcrumbs: any;
-export default Breadcrumbs;
+export interface BreadcrumbsProps extends WixComponentProps {
+  items: BreadcrumbsItem[];
+  onClick?: (item: BreadcrumbsItem) => void;
+  activeId?: string | number;
+  size?: BreadcrumbsSize;
+  theme?: BreadcrumbsTheme;
+}
+
+export default class Breadcrumbs extends WixComponent<BreadcrumbsProps> {}
+
+export interface BreadcrumbsItem {
+  id: string | number;
+  value: React.ReactNode;
+  link?: string;
+  customElement?: JSX.Element;
+  disabled?: boolean;
+}
+
+export type BreadcrumbsSize = 'medium' | 'large';
+
+export type BreadcrumbsTheme =
+  | 'onWhiteBackground'
+  | 'onGrayBackground'
+  | 'onDarkBackground';
