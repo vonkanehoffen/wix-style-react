766c52a1f0fedeeb923f3dd0de5ee92f9c179dbf
Gilad Segal <gilads@wix.com>
2019-04-30 16:32:19 +0300

    Add DatePicker types

--- src/DatePicker/DatePicker.driver.d.ts
+++ src/DatePicker/DatePicker.driver.d.ts
@@ -1,1 +1,11 @@
-export type DatePickerDriver = any;
+import {BaseDriver} from "wix-ui-test-utils/driver-factory";
+import {CalendarDriver} from "../Calendar/Calendar.driver";
+
+export interface DatePickerDriver extends BaseDriver {
+    driver: {
+        exists: () => boolean,
+        open: () => void;
+    },
+    inputDriver: any, // todo: add once input types are implemented
+    calendarDriver: CalendarDriver
+}
--- src/DatePicker/index.d.ts
+++ src/DatePicker/index.d.ts
@@ -1,4 +1,22 @@
-export type DatePickerProps = any;
+import WixComponent, {WixComponentProps} from '../BaseComponents/WixComponent';
+import {CalendarProps, CalendarLocale} from '../Calendar';
 
-declare const DatePicker: any;
-export default DatePicker;
+export type DatePickerProps = CalendarProps & WixComponentProps & {
+  customInput?: React.ReactNode;
+  inputProps?: any; // todo: add once Input types are implemented
+  dateFormat?: string | ((date: Date) => string);
+  disabled?: boolean;
+  inputDataHook?: string;
+  calendarDataHook?: string;
+  placeholderText?: string;
+  rtl?: boolean;
+  value?: Date | string;
+  isOpen?: boolean;
+  initialOpen?: boolean;    
+  error?: boolean;
+  errorMessage?: React.ReactNode;
+  width?: number;
+  zIndex?: number;
+}
+
+export default class DatePicker extends WixComponent<DatePickerProps> {}
