c3c2bc6ab1efec7d61406188911827d8efab04b5
Gilad Segal <gilads@wix.com>
2019-04-30 14:27:46 +0300

    Add CardGalleryItem types

--- src/CardGalleryItem/CardGalleryItem.uni.driver.d.ts
+++ src/CardGalleryItem/CardGalleryItem.uni.driver.d.ts
@@ -1,1 +1,13 @@
-export type CardGalleryItemDriver= any;
+import {BaseUniDriver} from "wix-ui-test-utils/unidriver";
+
+export interface CardGalleryItemDriver extends BaseUniDriver {
+    getTitle: () => Promise<string>,
+    getSubtitle: () => Promise<string>,
+    getBackgroundImageUrl: () => Promise<RegExpMatchArray | null>
+    getPrimaryActionLabel: () => Promise<string>,
+    clickOnPrimaryAction: () => Promise<void>,
+    getSecondaryActionLabel: () => Promise<String>,
+    clickOnSecondaryAction: () => Promise<void>,
+    getHoverComponent: () => Promise<any>,
+    getHoveredContent: () => Promise<any>
+}
--- src/CardGalleryItem/index.d.ts
+++ src/CardGalleryItem/index.d.ts
@@ -1,4 +1,19 @@
-export type CardGalleryItemProps = any;
+import * as React from 'react';
 
-declare const CardGalleryItem: any;
-export default CardGalleryItem;
+export interface CardGalleryItemProps {
+  primaryActionProps: {
+    label?: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLElement>;
+  };
+  secondaryActionProps: {
+    label?: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLElement>;
+  };
+  title?: React.ReactNode;
+  subtitle?: React.ReactNode;
+  backgroundImageUrl?: string;
+  className?: string;
+  dataHook?: string;
+}
+
+export default class CardGalleryItem extends React.Component<CardGalleryItemProps> {}
