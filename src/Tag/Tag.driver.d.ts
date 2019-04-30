6b2f79f5556d4327061d6995faefebb53ae85c54
Amit Dahan <amitda@wix.com>
2019-04-30 15:51:12 +0300

    Add Tag types

--- src/Tag/Tag.driver.d.ts
+++ src/Tag/Tag.driver.d.ts
@@ -1,1 +1,17 @@
-export type TagDriver = any;
+import { BaseDriver } from 'wix-ui-test-utils/driver-factory';
+
+export interface TagDriver extends BaseDriver {
+    isTiny(): boolean;
+    isSmall(): boolean;
+    isMedium(): boolean;
+    isLarge(): boolean;
+    isStandardTheme(): boolean;
+    isWarningTheme(): boolean;
+    isErrorTheme(): boolean;
+    isRemovable(): boolean;
+    removeTag(): Promise<void>;
+    click(): void;
+    isThumbExists(): boolean;
+    isDisabled(): boolean;
+    getLabel(): string;
+}
--- src/Tag/index.d.ts
+++ src/Tag/index.d.ts
@@ -1,4 +1,21 @@
-export type TagProps = any;
+import WixComponent, { WixComponentProps } from '../BaseComponents/WixComponent';
+import * as React from 'react';
 
-declare const Tag: any;
-export default Tag;
+export interface TagProps extends WixComponentProps {
+  disabled?: boolean;
+  id: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+  onRemove?(id: string): void;
+  removable?: boolean;
+  size?: TagSize;
+  theme?: TagTheme;
+  thumb?: React.ReactNode;
+  maxWidth?: number;
+  wrap?: boolean;
+  className?: string;
+}
+
+export type TagSize = 'tiny' | 'small' | 'medium' | 'large';
+export type TagTheme = 'standard' | 'error' | 'warning';
+
+export default class Tag extends WixComponent<TagProps> {}
