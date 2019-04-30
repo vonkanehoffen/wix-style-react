d312107cb9996b05d9ff9b79d6ed3c9fec713e07
Gilad Segal <gilads@wix.com>
2019-04-30 15:53:56 +0300

    Add ColorPicker types

--- src/ColorPicker/ColorPicker.driver.d.ts
+++ src/ColorPicker/ColorPicker.driver.d.ts
@@ -1,1 +1,10 @@
-export type ColorPickerDriver = any;
+import {BaseDriver} from 'wix-ui-test-utils/driver-factory';
+
+export interface ColorPickerDriver extends BaseDriver {
+  confirm: () => void;
+  cancel: () => void;
+  clickOnPreviousColor: () => void;
+  historyPanelExists: () => boolean;
+  historyCurrentColor: () => CSSStyleDeclaration['background'];
+  historyPreviousColor: () => CSSStyleDeclaration['background'];
+}
