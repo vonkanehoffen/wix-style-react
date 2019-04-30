a58d645e0594c5105132aba0f43b78e60f731c85
Amit Dahan <amitda@wix.com>
2019-04-30 14:50:01 +0300

    Add TextButton types

--- src/TextButton/TextButton.uni.driver.d.ts
+++ src/TextButton/TextButton.uni.driver.d.ts
@@ -1,1 +1,8 @@
-export type TextButtonDriver = any;
+import { BaseUniDriver, StylableUnidriverUtil } from 'wix-ui-test-utils/unidriver';
+import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
+import button from 'wix-ui-core/dist/src/components/button-next/button-next.st.css';
+
+export interface TextButtonDriver extends BaseUniDriver {
+  getButtonTextContent(): Promise<string>;
+  isButtonDisabled(): Promise<boolean>;
+}
--- src/TextButton/index.d.ts
+++ src/TextButton/index.d.ts
@@ -1,4 +1,17 @@
-export type TextButtonProps = any;
+import * as React from 'react';
+import { ButtonProps } from '../Button';
 
-declare const TextButton: any;
-export default TextButton;
+export interface TextButtonProps extends ButtonProps {
+  skin?: TextButtonSkin;
+  underline?: TextButtonUnderline;
+  weight?: TextButtonWeight;
+  size?: TextButtonSize;
+  target?: string;
+}
+
+export type TextButtonSkin = 'standard' | 'light' | 'premium' | 'dark';
+export type TextButtonUnderline = 'none' | 'onHover' | 'always';
+export type TextButtonWeight = 'thin' | 'normal';
+export type TextButtonSize = 'small' | 'medium';
+
+export default class TextButton extends React.Component<TextButtonProps> {}
