ee39e3077a02026c1c017fb1a6c08b1a50ba293a
Gilad Segal <gilads@wix.com>
2019-04-30 21:13:20 +0300

    Add EmptyState types

--- src/EmptyState/EmptyState.driver.d.ts
+++ src/EmptyState/EmptyState.driver.d.ts
@@ -1,1 +1,12 @@
-export type EmptyStateDriver = any;
+import {BaseDriver} from 'wix-ui-test-utils/driver-factory';
+import {EmptyStateTheme} from '.';
+
+export interface EmptyStateDriver extends BaseDriver {
+  element: () => HTMLElement;
+  getTitleText: () => string;
+  getSubtitleText: () => string;
+  hasTheme: (themeName: EmptyStateTheme) => boolean;
+  getImageUrl: () => string;
+  imageNodeExists: () => boolean;
+  childrenContentExists: () => boolean;
+}
--- src/EmptyState/index.d.ts
+++ src/EmptyState/index.d.ts
@@ -1,4 +1,14 @@
-export type EmptyStateProps = any;
+import * as React from 'react';
 
-declare const EmptyState: any;
+export interface EmptyStateProps {
+  theme?: EmptyStateTheme;
+  title?: string | React.ReactNode;
+  subtitle?: string | React.ReactNode;
+  image?: string | JSX.Element;
+  dataHook?: string;
+}
+
+declare const EmptyState: React.SFC<EmptyStateProps>;
 export default EmptyState;
+
+export type EmptyStateTheme = 'page' | 'page-no-border' | 'section';
