4ea013130a8d146cd2d4f27517b94ef44e4720a8
Amit Dahan <amitda@wix.com>
2019-04-30 14:35:36 +0300

    Add Thumbnail types

--- src/Thumbnail/Thumbnail.uni.driver.d.ts
+++ src/Thumbnail/Thumbnail.uni.driver.d.ts
@@ -1,1 +1,13 @@
-export type ThumbnailDriver = any;
+import { BaseUniDriver } from 'wix-ui-test-utils/base-driver';
+
+export interface ThumbnailDriver extends BaseUniDriver {
+  getTitle(): Promise<string>;
+  getDescription(): Promise<string>;
+  getSelectedIcon(): Promise<any>;
+  getBackgroundImage(): Promise<any>;
+  isSelected(): Promise<boolean>;
+  isDisabled(): Promise<boolean>;
+  getImage(): Promise<any>;
+  getWidth(): Promise<string>;
+  getHeight(): Promise<string>;
+}
--- src/Thumbnail/index.d.ts
+++ src/Thumbnail/index.d.ts
@@ -1,4 +1,21 @@
-export type ThumbnailProps = any;
+import * as React from 'react';
+import { InjectedFocusableProps } from '../index';
 
-declare const Thumbnail: any;
-export default Thumbnail;
+export interface ThumbnailProps extends InjectedFocusableProps {
+  dataHook?: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  image?: React.ReactNode;
+  size?: ThumbnailSize;
+  selected?: boolean;
+  disabled?: boolean;
+  hideSelectedIcon?: boolean;
+  backgroundImage?: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLDivElement>,
+  width?: React.CSSProperties['width'];
+  height?: React.CSSProperties['height'];
+}
+
+export type ThumbnailSize = 'tiny' | 'small' | 'medium';
+
+export default class Thumbnail extends React.PureComponent<ThumbnailProps> {}
