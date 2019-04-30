74ecd8c9e8c0382aefcae823db8cbbea1598a5af
Gilad Segal <gilads@wix.com>
2019-04-30 13:48:55 +0300

    Add Card types

--- src/Card/index.d.ts
+++ src/Card/index.d.ts
@@ -1,4 +1,31 @@
-export type CardProps = any;
+import * as React from 'react';
+import WixComponent, {WixComponentProps} from '../BaseComponents/WixComponent';
 
-declare const Card: any;
+export interface CardProps {
+  stretchVertically?: boolean;
+  hideOverflow?: boolean;
+  dataHook?: string;
+  children?: React.ReactNode
+}
+
+declare const Card: {
+  (props: CardProps): JSX.Element | null;
+  Content: typeof Content;
+  Header: typeof Header;
+  Divider: typeof Divider;
+};
+
 export default Card;
+
+declare class Content extends React.Component {}
+
+declare class Header extends WixComponent<HeaderProps> {}
+
+declare const Divider: React.SFC;
+
+interface HeaderProps extends WixComponentProps {
+  title: React.ReactNode;
+  subtitle?: React.ReactNode;
+  suffix?: React.ReactNode;
+  withoutDivider?: boolean;
+}
