10131302b9bb56e74512764870358e71ca659b9e
Gilad Segal <gilads@wix.com>
2019-04-30 13:27:06 +0300

    Add CalendarPanelFooter types

--- src/CalendarPanelFooter/CalendarPanelFooter.uni.driver.d.ts
+++ src/CalendarPanelFooter/CalendarPanelFooter.uni.driver.d.ts
@@ -1,1 +1,10 @@
-export type CalendarPanelFooterDriver = any;
+import {BaseUniDriver} from 'wix-ui-test-utils/unidriver';
+
+export interface CalendarPanelFooterDriver extends BaseUniDriver {
+  isPrimaryButtonDisabled: boolean;
+  getSelectedDaysText: () => string;
+  getPrimaryActionButtonLabel: () => string;
+  getSecondaryActionButtonLabel: () => string;
+  clickOnPrimaryButton: () => void;
+  clickOnSecondaryButton: () => void;
+}
--- src/CalendarPanelFooter/index.d.ts
+++ src/CalendarPanelFooter/index.d.ts
@@ -1,4 +1,17 @@
-export type CalendarPanelFooterProps = any;
+import * as React from 'react';
+import {CalendarValue} from '../Calendar';
 
-declare const CalendarPanelFooter: any;
-export default CalendarPanelFooter;
+export interface CalendarPanelFooterProps {
+  primaryActionLabel: string;
+  secondaryActionLabel: string;
+  primaryActionDisabled: boolean;
+  primaryActionOnClick: React.MouseEventHandler<HTMLElement>;
+  secondaryActionOnClick: React.MouseEventHandler<HTMLElement>;
+  dateToString: (date: Date) => string;
+  selectedDays?: CalendarValue;
+  dataHook?: string;
+}
+
+export default class CalendarPanelFooter extends React.PureComponent<
+  CalendarPanelFooterProps
+> {}
