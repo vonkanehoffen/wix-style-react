04c18215bc1510f8bd4e2e6864c4b1486f078276
Gilad Segal <gilads@wix.com>
2019-04-30 13:05:09 +0300

    Add CalendarPanel types

--- src/CalendarPanel/CalendarPanel.driver.d.ts
+++ src/CalendarPanel/CalendarPanel.driver.d.ts
@@ -1,1 +1,9 @@
-export type CalendarPanelDriver = any;
+import {BaseDriver} from 'wix-ui-test-utils/driver-factory';
+import {CalendarDriver} from '../Calendar/Calendar.driver';
+
+export interface CanlendarPanelDriver extends BaseDriver {
+    calendarDriver: () => CalendarDriver,
+    presetsDropdownLayoutDriver: () => any, // todo: dropdown layout driver
+    isDropdownExists: () => boolean,
+    findByDataHook: (dataHook: string) => HTMLElement | null
+}
