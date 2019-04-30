7b605945ffabfaba41ada161b2dc722124f2ec7c
Gilad Segal <gilads@wix.com>
2019-04-30 21:52:10 +0300

    Add FilePicker types

--- src/FilePicker/FilePicker.driver.d.ts
+++ src/FilePicker/FilePicker.driver.d.ts
@@ -1,1 +1,10 @@
-export type FilePickerDriver = any;
+import {BaseDriver} from "wix-ui-test-utils/driver-factory";
+
+export interface FilePickerDriver extends BaseDriver {
+    hasError: () => boolean,
+    errorMessage: () => string,
+    getInput: () => string,
+    getSubLabel: () => string,
+    getMainLabel: () => string,
+    getName: () => string,
+}
--- src/FilePicker/index.d.ts
+++ src/FilePicker/index.d.ts
@@ -1,4 +1,16 @@
-export type FilePickerProps = any;
+import WixComponent, {WixComponentProps} from "../BaseComponents/WixComponent";
 
-declare const FilePicker: any;
-export default FilePicker;
+export interface FilePickerProps extends WixComponentProps {
+    header?: string;
+    onChange?: (file: File) => void;
+    mainLabel?: string;
+    subLabel?: string;
+    supportedFormats?: string;
+    maxSize?: number;
+    error?: boolean;
+    errorMessage?: string;
+    id?: string | number;
+    name?: string;
+  }
+
+  export default class FilePicker extends WixComponent<FilePickerProps> {}
