012c4858952b05a365a0a6884f486dfd92f2d7a9
Gilad Segal <gilads@wix.com>
2019-04-30 20:58:33 +0300

    Add EditableSelector types

--- src/EditableSelector/index.d.ts
+++ src/EditableSelector/index.d.ts
@@ -1,4 +1,24 @@
-export type EditableSelectorProps = any;
+import WixComponent, {WixComponentProps} from '../BaseComponents/WixComponent';
 
-declare const EditableSelector: any;
-export default EditableSelector;
+export interface EditableSelectorProps extends WixComponentProps {
+  title?: string;
+  toggleType?: EditableSelectorToggleType;
+  newRowLabel?: string;
+  editButtonText?: string;
+  onOptionAdded?: (data: {newTitle: string}) => void;
+  onOptionEdit?: (data: {newTitle: string; index: number}) => void;
+  onOptionDelete?: (data: {index: number}) => void;
+  onOptionToggle?: (id: number) => void;
+  options?: EditableSelectorOption[];
+}
+
+export default class EditableSelector extends WixComponent<
+  EditableSelectorProps
+> {}
+
+export type EditableSelectorToggleType = 'checkbox' | 'radio';
+
+export interface EditableSelectorOption {
+  title: string;
+  isSelected?: boolean;
+}
