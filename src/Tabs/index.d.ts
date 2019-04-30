b08cba6699a4cab1e482438f3a952726c52b32f5
Amit Dahan <amitda@wix.com>
2019-04-30 16:22:31 +0300

    Add Tabs types

--- src/Tabs/index.d.ts
+++ src/Tabs/index.d.ts
@@ -1,4 +1,24 @@
-export type TabsProps = any;
+import * as React from 'react';
+import WixComponent, { WixComponentProps } from '../BaseComponents/WixComponent';
+import { CSSProperties } from 'react';
 
-declare const Tabs: any;
-export default Tabs;
+interface Tab {
+  id: string | number,
+  title: React.ReactNode;
+  dataHook?: string;
+}
+
+export interface TabsProps extends WixComponentProps {
+  activeId?: string | number;
+  hasDivider?: boolean;
+  items: Tab[];
+  minWidth?: CSSProperties['minWidth'];
+  type?: TabsType;
+  sideContent?: React.ReactNode;
+  width?: string | number;
+  onClick?(tab: Tab): TabPropTypes.onClick,
+}
+
+export type TabsType = '' | 'compact' | 'compactSide' | 'uniformSide' | 'uniformFull';
+
+export default class Tabs extends WixComponent<TabsProps> {}
