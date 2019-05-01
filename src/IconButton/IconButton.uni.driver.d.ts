20af90f27d3d1faa2de76748ef78e90bf3f1a96f
Gilad Segal <gilads@wix.com>
2019-05-01 19:45:02 +0300

    Add IconButton types

--- src/IconButton/IconButton.uni.driver.d.ts
+++ src/IconButton/IconButton.uni.driver.d.ts
@@ -1,1 +1,5 @@
-export type IconButtonDriver = any;
+import {BaseUniDriver} from "wix-ui-test-utils/unidriver";
+
+export interface IconButtonDriver extends BaseUniDriver {
+    isButtonDisabled: () => Promise<boolean>;
+}
--- src/IconButton/index.d.ts
+++ src/IconButton/index.d.ts
@@ -1,4 +1,20 @@
-export type IconButtonProps = any;
+import * as React from 'react';
 
-declare const IconButton: any;
-export default IconButton;
+export interface IconButtonProps {
+  as?: any;
+  className?: string;
+  skin?: IconButtonSkin;
+  priority?: IconButtonPriority;
+  size?: IconButtonSize;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+  disabled?: boolean;
+  dataHook?: string;
+}
+
+export default class IconButton extends React.Component<IconButtonProps> {}
+
+export type IconButtonSkin = 'standard' | 'inverted' | 'light';
+
+export type IconButtonPriority = 'primary' | 'secondary';
+
+export type IconButtonSize = 'small' | 'medium';
