c18d888a8a9163adb5106773530d01f755839e55
Gilad Segal <gilads@wix.com>
2019-05-01 18:26:48 +0300

    Add FormField types

--- src/FormField/FormField.driver.d.ts
+++ src/FormField/FormField.driver.d.ts
@@ -1,1 +1,12 @@
-export type FormFieldDriver = any;
+import {BaseDriver} from 'wix-ui-test-utils/driver-factory';
+import {TooltipDriver} from '../Tooltip/Tooltip.uni.driver';
+
+export interface FormFieldDriver extends BaseDriver {
+  element: () => HTMLElement;
+  getChildren: () => HTMLElement;
+  getLabel: () => HTMLElement;
+  isRequired: () => boolean;
+  getLengthLeft: () => number | null;
+  isLengthExceeded: () => boolean;
+  getInfoContent: () => TooltipDriver['hoverAndGetContent'];
+}
--- src/FormField/index.d.ts
+++ src/FormField/index.d.ts
@@ -1,4 +1,25 @@
-export type FormFieldProps = any;
+import * as React from 'react';
+import {TooltipProps} from '../Tooltip';
 
-declare const FormField: any;
-export default FormField;
+export interface FormFieldProps {
+  children?:
+    | React.ReactNode
+    | ((data: {setCharactersLeft: CharactersLeftFn}) => React.ReactNode);
+  stretchContent?: boolean;
+  label?: React.ReactNode;
+  labelSize?: LabelPlacement;
+  labelPlacement?: FormFieldLabelPlacement;
+  required?: boolean;
+  infoContent?: React.ReactNode;
+  infoTooltipProps?: TooltipProps;
+  id?: string;
+  dataHook?: string;
+}
+
+export default class FormField extends React.Component<FormFieldProps> {}
+
+export type FormFieldLabelPlacement = 'top' | 'right' | 'left';
+
+export type LabelPlacement = 'small' | 'medium';
+
+type CharactersLeftFn = (lengthLeft: number) => void;
