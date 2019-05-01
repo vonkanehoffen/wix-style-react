f5be695d0dc44e25855a685a9328bb15452af4cf
Gilad Segal <gilads@wix.com>
2019-05-01 19:26:15 +0300

    Add Heading types

--- src/Heading/index.d.ts
+++ src/Heading/index.d.ts
@@ -1,4 +1,11 @@
-export type HeadingProps = any;
+import * as React from 'react';
 
-declare const Heading: any;
+export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  light?: boolean;
+  appearance?: HeadingAppearance;
+}
+
+declare const Heading: React.SFC<HeadingProps>;
 export default Heading;
+
+export type HeadingAppearance = 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6';
