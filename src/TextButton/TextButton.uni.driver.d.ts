9fff0d38666bd4c00122b81dc0c1f0d1e966f2c6
Amit Dahan <amitda@wix.com>
2019-05-02 15:20:04 +0300

    Fix TextButton types

--- src/TextButton/TextButton.uni.driver.d.ts
+++ src/TextButton/TextButton.uni.driver.d.ts
@@ -1,6 +1,4 @@
-import { BaseUniDriver, StylableUnidriverUtil } from 'wix-ui-test-utils/unidriver';
-import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
-import button from 'wix-ui-core/dist/src/components/button-next/button-next.st.css';
+import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface TextButtonDriver extends BaseUniDriver {
   getButtonTextContent(): Promise<string>;
