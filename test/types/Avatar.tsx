d2001f9b58faf81717795aa4c95cd907832f5408
Amit Dahan <amitda@wix.com>
2019-05-02 11:26:45 +0300

    Infra - add type definitions - add Avatar types (#3332)
    * Add types infrastructure
    
    * Add default types for everything untyped
    
    * Export definition files for icons
    
    * Revert "Export definition files for icons"
    
    This reverts commit a757b5b3

---
+++ test/types/Avatar.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+import Avatar from '../../src/Avatar';
+import {avatarTestkitFactory} from '../../testkit';
+import {avatarTestkitFactory as avatarEnzymeTestkitFactory} from '../../testkit/enzyme';
+
+async function testkits() {
+  const vanilla = avatarTestkitFactory({
+    dataHook: 'hi',
+    wrapper: document.createElement('div')
+  });
+
+  await vanilla.exists();
+  await vanilla.getTextContent();
+
+  const enzyme = avatarEnzymeTestkitFactory({
+    dataHook: 'shbem',
+    wrapper: mount(<div />)
+  });
+}
+
+function AvatarWithMandatoryProps() {
+  return <Avatar />;
+}
+
+function AvatarWithAllProps() {
+  return (
+    <Avatar
+      className="some-class"
+      name="as"
+      size="size90"
+      ariaLabel="asasas"
+      color="blue"
+      dataHook="sadasd"
+      imgProps={{alt: 'asd'}}
+      placeholder="blalbal"
+      text="some text"
+      title="some title"
+    />
+  );
+}
