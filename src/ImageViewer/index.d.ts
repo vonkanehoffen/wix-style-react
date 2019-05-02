edc5ada94d87e77b5a47b3f47869d86f916118d1
Gilad Segal <gilads@wix.com>
2019-05-02 09:41:57 +0300

    Add ImageViewer types

--- src/ImageViewer/index.d.ts
+++ src/ImageViewer/index.d.ts
@@ -1,4 +1,27 @@
-export type ImageViewerProps = any;
+import WixComponent, {WixComponentProps} from "../BaseComponents/WixComponent";
+import {TooltipProps} from "../Tooltip";
+import {Omit} from "..";
 
-declare const ImageViewer: any;
-export default ImageViewer;
+export interface ImageViewerProps extends WixComponentProps {
+  imageUrl?: string;
+  error?: boolean;
+  errorMessage?: string;
+  /** Error tooltp placement
+   * @deprecated
+   * @see tooltipProps
+   */
+  tooltipPlacement?: TooltipProps['placement'];
+  tooltipProps?: Omit<TooltipProps, 'content'>;
+  showUpdateButton?: boolean; 
+  onAddImage?: React.MouseEventHandler<HTMLElement>;
+  onUpdateImage?: React.MouseEventHandler<HTMLElement>;
+  onRemoveImage?: React.MouseEventHandler<HTMLElement>;
+  addImageInfo?: string;
+  updateImageInfo?: string;
+  removeImageInfo?: string;
+  removeRoundedBorders?: boolean;
+  width?: number | string;
+  height?: number | string;
+}
+
+export default class ImageViewer extends WixComponent<ImageViewerProps> {}
