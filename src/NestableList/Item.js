6e347be614ca96e2205328c4260a2b5ed56fd211
Amit Dahan <amitdahan11@gmail.com>
2019-05-06 12:27:37 +0300

    Fix bad import of PropTypes (#3359)

--- src/NestableList/Item.js
+++ src/NestableList/Item.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import WixComponent from '../BaseComponents/WixComponent';
 import { findDOMNode } from 'react-dom';
 import shallowCompare from 'react-addons-shallow-compare';
