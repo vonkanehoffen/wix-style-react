1473a09f9db48ed98f02da2ae1630f6989508a94
Shlomi Toussia-Cohen <shlomitc@wix.com>
2019-05-08 18:16:55 +0300

    Badge - fix skin visual tests failure

--- src/Badge/tests/Badge.visual.js
+++ src/Badge/tests/Badge.visual.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import ExampleBadges from '../docs/ExampleBadges';
-import Badge from '..';
+import Badge, { SIZE, SKIN, TYPE } from '..';
 
 storiesOf('Badge', module).add('base', () => <Badge>I'M A BADGE!</Badge>);
-storiesOf('Badge', module).add('skins', () => <ExampleBadges />);
+storiesOf('Badge', module).add('skins', () => <BadgesVariations />);
 storiesOf('Badge', module).add('focus', () => <FocusBadgeTest />);
+
+const skins = Object.keys(SKIN);
+const sizes = Object.keys(SIZE);
+const types = Object.keys(TYPE);
+
+const renderBadge = props => (
+  <span style={{ padding: '5px' }}>
+    <Badge {...props}>Some Badge</Badge>
+  </span>
+);
+
+const renderTypes = props => types.map(type => renderBadge({ type, ...props }));
+const renderSizes = props => sizes.map(size => renderTypes({ size, ...props }));
+
+class BadgesVariations extends React.Component {
+  render() {
+    return (
+      <div>
+        {skins.map(skin => (
+          <div key={skin}>
+            <div
+              style={{ display: 'flex', alignItems: 'center', padding: '5px' }}
+              key={skin}
+            >
+              {renderSizes({ skin })}
+              {renderBadge({ uppercase: false, skin })}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 class FocusBadgeTest extends React.Component {
   componentDidMount() {
