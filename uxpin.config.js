e749b9c605d2958aa650fa163688f139665ea15a
Andrius Kripaitis <andriusk@wix.com>
2019-05-27 11:55:31 +0300

    Add uxpin config;

---
+++ uxpin.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'General',
+        include: ['src/Button/Button.js'],
+      },
+    ],
+  },
+  name: 'New Design System',
+};
