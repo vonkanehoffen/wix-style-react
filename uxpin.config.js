6656fa145626325928a0926643e063650e2e8a6f
Andrius Kripaitis <andriusk@wix.com>
2019-05-29 11:56:54 +0300

    Add AddItem;

--- uxpin.config.js
+++ uxpin.config.js
@@ -4,6 +4,7 @@ module.exports = {
       {
         name: 'General',
         include: [
+          'src/AddItem/AddItem.js',
           'src/Badge/Badge.js',
           'src/Button/Button.js',
           'src/Calendar/Calendar.js',
