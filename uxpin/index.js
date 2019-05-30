863f6abf3d68202bf270370bb93b06c77097f029
Andrius Kripaitis <andriusk@wix.com>
2019-05-30 11:34:39 +0300

    Revert "Build dynamic uxpin config;"
    This reverts commit 0cb04adb4ae78fc42a1c8ef8e7ee1ea1abb20800.

--- uxpin/index.js
+++ uxpin/index.js
@@ -1,31 +1,10 @@
 const extractPropsMetadata = require('./extractPropsMetadata');
 const buildComponentWrappers = require('./buildComponentWrappers');
-const util = require('util');
-const fs = require('fs');
-
-const writeFile = util.promisify(fs.writeFile);
 
 extractMetadataAndBuildComponentWrappers();
-
-function buildUxpinConfig(includes, configFile) {
-  return writeFile(
-    configFile,
-    `module.exports = {
-    components: {
-      categories: [
-        {
-          name: 'General',
-          include: ${JSON.stringify(includes)}
-        },
-      ],
-    },
-    name: 'WSR Design System',
-  };`,
-  );
-}
 
 async function extractMetadataAndBuildComponentWrappers() {
   const metadata = await extractPropsMetadata();
   const uxPinIncludes = await buildComponentWrappers(metadata);
-  await buildUxpinConfig(uxPinIncludes, './dist/uxpin/uxpin.config.js');
+  console.log(JSON.stringify(uxPinIncludes)); // eslint-disable-line no-console
 }
