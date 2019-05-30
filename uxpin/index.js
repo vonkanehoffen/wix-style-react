fc77e24b260078646da0fb4bbda0c9f4ea227291
Andrius Kripaitis <andriusk@wix.com>
2019-05-30 13:08:46 +0300

    Restore uxpin config generator;

--- uxpin/index.js
+++ uxpin/index.js
@@ -1,10 +1,31 @@
 const extractPropsMetadata = require('./extractPropsMetadata');
 const buildComponentWrappers = require('./buildComponentWrappers');
+const util = require('util');
+const fs = require('fs');
+
+const writeFile = util.promisify(fs.writeFile);
 
 extractMetadataAndBuildComponentWrappers();
+
+function buildUxpinConfig(includes, configFile) {
+  return writeFile(
+    configFile,
+    `module.exports = {
+    components: {
+      categories: [
+        {
+          name: 'General',
+          include: ${JSON.stringify(includes)}
+        },
+      ],
+    },
+    name: 'WSR Design System',
+  };`,
+  );
+}
 
 async function extractMetadataAndBuildComponentWrappers() {
   const metadata = await extractPropsMetadata();
   const uxPinIncludes = await buildComponentWrappers(metadata);
-  console.log(JSON.stringify(uxPinIncludes)); // eslint-disable-line no-console
+  await buildUxpinConfig(uxPinIncludes, './dist/uxpin/uxpin.config.js');
 }
