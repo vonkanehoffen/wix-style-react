e332add03984e40db972f77c38f6bd472b93a987
Vytautas Traškevičius <vytautast@wix.com>
2019-05-31 11:26:16 +0300

    add calendar preset

---
+++ uxpin/presets/Calendar/0-default.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calendar from '../Calendar';
+
+export default (
+  <Calendar
+    uxpId="calendar1"
+    selectionMode="range"
+    showMonthDropdown
+    showYearDropdown
+  />
+);
