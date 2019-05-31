0dba3c99624ca580f82639363c45703fca52badc
Andrius Kripaitis <andriusk@wix.com>
2019-05-31 11:21:46 +0300

    Added input;

---
+++ uxpin/presets/ImageViewer/0-default.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ImageViewer from '../ImageViewer';
+
+export default (
+  <ImageViewer
+    imageUrl="https://upload.wikimedia.org/wikipedia/commons/d/dd/New_Mela_Ramanputhur_Holy_Family_Church.jpg"
+    uxpId="imageviewer1"
+  />
+);
