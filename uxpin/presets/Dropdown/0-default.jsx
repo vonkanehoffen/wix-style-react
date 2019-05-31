d51fb16f1043bff1a33c234e0d8c3a4248df9496
Andrius Kripaitis <andriusk@wix.com>
2019-05-31 11:38:01 +0300

    Added Dropdown;

---
+++ uxpin/presets/Dropdown/0-default.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Dropdown from '../Dropdown';
+
+export default (
+  <Dropdown
+    options={[
+      {
+        id: 0,
+        value: 'Option 1'
+      },
+      {
+        id: 1,
+        value: 'Option 2'
+      },
+      {
+        id: 2,
+        value: 'Option 3'
+      },
+      {
+        id: 3,
+        value: 'Option 4'
+      }
+    ]}
+    placeholder="This is a placeholder"
+  />
+);
