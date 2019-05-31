f60a4382cad4b6ccc94bae5d9e0467fcd008dd34
Vytautas Traškevičius <vytautast@wix.com>
2019-05-31 11:13:21 +0300

    add presets for AddItem, Badge

---
+++ uxpin/presets/AddItem/0-default.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import AddItem from '../AddItem';
+
+export default <AddItem uxpId="addItem1">Add Item</AddItem>;
---
+++ uxpin/presets/Badge/0-default.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import Badge from '../Badge';
+
+export default <Badge size="small">small badge</Badge>;
