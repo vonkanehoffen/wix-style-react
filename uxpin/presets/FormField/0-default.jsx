01108fc8e2067c1bcfb79f7ab499cb9031c053dd
Andrius Kripaitis <andriusk@wix.com>
2019-05-31 11:35:07 +0300

    Added FormFirld;

---
+++ uxpin/presets/FormField/0-default.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import FormField from '../FormField';
+import Input from '../../Input/Input';
+
+export default (
+  <FormField label="Label" labelPlacement="left" uxpId="formfield1">
+    <Input size="normal" placeholder="Placeholder" uxpId="input1" />
+  </FormField>
+);
