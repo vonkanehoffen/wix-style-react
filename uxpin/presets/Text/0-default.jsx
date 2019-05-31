58a706e85f2c798663fc4f6622405cf03b51dde1
Andrius Kripaitis <andriusk@wix.com>
2019-05-31 10:39:00 +0300

    Added Text component presets;

---
+++ uxpin/presets/Text/0-default.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Text from '../Text';
+
+export default (
+    <Text
+        uxpId="text1"
+        size="medium"
+        skin="standard"
+        tagName="span"
+        weight="thin"
+    >Some text
+    </Text>
+);
