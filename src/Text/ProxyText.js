660ad5acfdd02f514ad85f022e340d40320701ab
mykas <mykolass@wix.com>
2019-06-16 16:05:12 +0300

    <Text/> - move appendTo to tooltipProps object

--- src/Text/ProxyText.js
+++ src/Text/ProxyText.js
@@ -6,8 +6,7 @@ import tooltip from '../Tooltip/TooltipNext/Tooltip.st.css';
 
 const EllipsedText = withEllipsedTooltip({
   showTooltip: true,
-  tooltipProps: { className: tooltip.root },
-  appendTo: 'window',
+  tooltipProps: { className: tooltip.root, appendTo: 'window' },
 })(OriginalText);
 
 const Text = ({ ellipsis, ...props }) =>
