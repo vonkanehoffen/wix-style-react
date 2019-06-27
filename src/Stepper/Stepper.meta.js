437160f4fa1ba6db71ffb78b990513fad449408a
moshek <moshek@wix.com>
2019-06-27 13:36:16 +0300

    using ui-autotools ssr test for stepper

--- src/Stepper/Stepper.meta.js
+++ src/Stepper/Stepper.meta.js
@@ -6,3 +6,23 @@ const metadata = Registry.getComponentMetadata(Stepper);
 metadata.exportedFrom({
   path: 'src/Stepper/Stepper.js',
 });
+
+metadata.addSim({
+  title: 'Simulation with default props',
+  props: {
+    steps: [
+      { text: 'This is a long step name' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+    ],
+    onClick: () => {},
+    activeStep: 0,
+  },
+});
+
+metadata.nonReactStrictModeCompliant = true;
+metadata.nonEventListenerTestCompliant = true; // disabling because this test does not work well with React15
