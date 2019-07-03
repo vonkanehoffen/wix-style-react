7640e40daafcadb54cbaa65d33956784c7c3eaa3
mosheKerbel <moshek@wix.com>
2019-07-03 18:12:47 +0300

    Infra - use ui-autotools for ssr sanity tests (#3666)
    * use ui-autotools for ssr sanity tests
    
    * change test desc

--- src/Stepper/Stepper.meta.js
+++ src/Stepper/Stepper.meta.js
@@ -2,7 +2,26 @@ import Stepper from './Stepper';
 import Registry from '@ui-autotools/registry';
 
 const metadata = Registry.getComponentMetadata(Stepper);
+metadata.nonEventListenerTestCompliant = true;
+metadata.nonReactStrictModeCompliant = true;
 
 metadata.exportedFrom({
   path: 'src/Stepper/Stepper.js',
+});
+
+metadata.addSim({
+  title: 'Simulation of long step texts that are rendered with ellipsis',
+  props: {
+    steps: [
+      { text: 'This is a long step name' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+      { text: 'This is a long step name', type: 'disabled' },
+    ],
+    onClick: () => {},
+    activeStep: 0,
+  },
 });
