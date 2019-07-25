bd657e3148ad4531d56eb71d7cd7835ac44bc4b2
Mykolas Sindeikis <mykolass@wix.com>
2019-07-25 13:42:08 +0300

    Specify bundles folder (#3838)

--- perfer.config.js
+++ perfer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   bundleSize: {
     files: [
-      './dist/*.js', // It can be an exact file
-      './dist/*.css', // or just use a glob
+      './bundles/*.js', // It can be an exact file
+      './bundles/*.css', // or just use a glob
       {
         glob: './bundles/Accordion.js',
         maxSize: '115kb', // You can add thresholds for your bundles to break the build due to the gzipped bundle size
