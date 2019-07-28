133d4c15fb3c51f3fdf43947c0a96a5422b6f6b1
mosheKerbel <moshek@wix.com>
2019-07-28 15:44:27 +0300

    add base uniDriver function (#3806)

--- src/AddItem/AddItem.uni.driver.js
+++ src/AddItem/AddItem.uni.driver.js
@@ -1,5 +1,6 @@
 import { textUniDriverFactory } from '../Text/Text.uni.driver';
 import { tooltipDriverFactory } from '../Tooltip/TooltipNext/Tooltip.uni.driver';
+import { baseUniDriverFactory } from 'wix-ui-test-utils/unidriver';
 
 export const addItemUniDriverFactory = (base, body) => {
   const tooltipSelector = '[data-hook*="additem-tooltip"]';
@@ -8,7 +9,7 @@ export const addItemUniDriverFactory = (base, body) => {
   const textDriver = textUniDriverFactory(base.$(textSelector));
 
   return {
-    click: () => base.click(),
+    ...baseUniDriverFactory(base),
     /** returns value of action text */
     getText: () => textDriver.getText(),
     /** true if passed children in string exists */
