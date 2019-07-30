cfea0d1c510a1400e72de2b4212c273d35b8fd57
moshek <moshek@wix.com>
2019-07-30 09:40:16 +0300

    create popover uniDriver

--- src/Popover/Popover.spec.js
+++ src/Popover/Popover.spec.js
@@ -1,80 +1,93 @@
 import React from 'react';
-import { createDriverFactory } from 'wix-ui-test-utils/driver-factory';
-
+import {
+  createRendererWithDriver,
+  createRendererWithUniDriver,
+  cleanup,
+} from '../../test/utils/react';
 import Popover from './Popover';
 import popoverDriverFactory from './Popover.driver';
+import popoverUniDriverFactory from './Popover.uni.driver';
 
 describe('Popover', () => {
-  const createDriver = createDriverFactory(popoverDriverFactory);
-
-  it("should inherit core's driver methods", () => {
-    const onClickFn = jest.fn();
-
-    const driver = createDriver(
-      <Popover shown onClick={onClickFn}>
-        <Popover.Element>I am the trigger!</Popover.Element>
-        <Popover.Content>I am the content!</Popover.Content>
-      </Popover>,
-    );
-
-    expect(driver.isTargetElementExists()).toBe(true);
-    expect(driver.isContentElementExists()).toBe(true);
-
-    driver.click();
-    expect(onClickFn).toHaveBeenCalledTimes(1);
+  describe('[sync]', () => {
+    runTests(createRendererWithDriver(popoverDriverFactory));
   });
 
-  describe('propTypes validation', () => {
-    let consoleErrorSpy;
+  describe('[async]', () => {
+    runTests(createRendererWithUniDriver(popoverUniDriverFactory));
+  });
 
-    beforeEach(() => {
-      consoleErrorSpy = jest
-        .spyOn(global.console, 'error')
-        .mockImplementation(jest.fn());
-    });
+  function runTests(render) {
+    afterEach(() => cleanup());
+    it("should inherit core's driver methods", async () => {
+      const onClickFn = jest.fn();
 
-    it('should throw a PropType error when not provided with Popover.Element', () => {
-      createDriver(
-        <Popover>
+      const { driver } = render(
+        <Popover shown onClick={onClickFn}>
+          <Popover.Element>I am the trigger!</Popover.Element>
           <Popover.Content>I am the content!</Popover.Content>
         </Popover>,
       );
 
-      expect(consoleErrorSpy).toHaveBeenCalledWith(
-        expect.stringContaining(
-          'Warning: Failed prop type: Invalid children provided, <Popover.Element/> must be provided',
-        ),
-      );
+      expect(await driver.isTargetElementExists()).toBe(true);
+      expect(await driver.isContentElementExists()).toBe(true);
+
+      await driver.click();
+      expect(onClickFn).toHaveBeenCalledTimes(1);
     });
 
-    it('should throw a PropType error when not provided with Popover.Content', () => {
-      createDriver(
-        <Popover>
-          <Popover.Element>I am the Element!</Popover.Element>
-        </Popover>,
-      );
+    describe('propTypes validation', () => {
+      let consoleErrorSpy;
 
-      expect(consoleErrorSpy).toHaveBeenCalledWith(
-        expect.stringContaining(
-          'Warning: Failed prop type: Invalid children provided, <Popover.Content/> must be provided',
-        ),
-      );
+      beforeEach(() => {
+        consoleErrorSpy = jest
+          .spyOn(global.console, 'error')
+          .mockImplementation(jest.fn());
+      });
+
+      it('should throw a PropType error when not provided with Popover.Element', async () => {
+        render(
+          <Popover>
+            <Popover.Content>I am the content!</Popover.Content>
+          </Popover>,
+        );
+
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            'Warning: Failed prop type: Invalid children provided, <Popover.Element/> must be provided',
+          ),
+        );
+      });
+
+      it('should throw a PropType error when not provided with Popover.Content', async () => {
+        render(
+          <Popover>
+            <Popover.Element>I am the Element!</Popover.Element>
+          </Popover>,
+        );
+
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            'Warning: Failed prop type: Invalid children provided, <Popover.Content/> must be provided',
+          ),
+        );
+      });
+
+      it('should throw a PropType error when provided with unknown child', async () => {
+        render(
+          <Popover>
+            <Popover.Element>I am the Element!</Popover.Element>
+            <Popover.Content>I am the content!</Popover.Content>
+            <div>Who am I? What am I?</div>
+          </Popover>,
+        );
+
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          expect.stringContaining(
+            'Warning: Failed prop type: Invalid children provided, unknown child <div/> supplied',
+          ),
+        );
+      });
     });
-
-    it('should throw a PropType error when provided with unknown child', () => {
-      createDriver(
-        <Popover>
-          <Popover.Element>I am the Element!</Popover.Element>
-          <Popover.Content>I am the content!</Popover.Content>
-          <div>Who am I? What am I?</div>
-        </Popover>,
-      );
-
-      expect(consoleErrorSpy).toHaveBeenCalledWith(
-        expect.stringContaining(
-          'Warning: Failed prop type: Invalid children provided, unknown child <div/> supplied',
-        ),
-      );
-    });
-  });
+  }
 });
---
+++ src/Popover/Popover.uni.driver.js
@@ -0,0 +1,3 @@
+import { testkit as popoverDriverFactory } from 'wix-ui-core/dist/src/components/popover/Popover.uni.driver';
+
+export default popoverDriverFactory;
