bc19bb93ac4667a1cd8f4956c9e22f8b9e82e30a
Mykhailo Kotsiievskyi <maxvektor@users.noreply.github.com>
2019-08-19 14:02:52 +0300

    StatisticsWidget - add more visual test (#3969)

--- src/StatisticsWidget/test/StatisticsWidget.visual.js
+++ src/StatisticsWidget/test/StatisticsWidget.visual.js
@@ -106,10 +106,10 @@ const tests = [
     ],
   },
   {
-    describe: 'short text',
+    describe: 'title',
     its: [
       {
-        it: 'without short text in value',
+        it: 'without short text in a value',
         props: {
           statistics: [
             {
@@ -128,7 +128,7 @@ const tests = [
         },
       },
       {
-        it: 'with short text on value',
+        it: 'with short text in a value',
         props: {
           statistics: [
             {
@@ -146,6 +146,113 @@ const tests = [
             {
               value: 'Testing very very long text',
               valueInShort: 'Text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    describe: 'description',
+    its: [
+      {
+        it: 'extra long description',
+        props: {
+          statistics: [
+            {
+              value: '$1,000',
+              description: 'The income out of trading goods on Monday',
+              descriptionInfo: 'Sales on Monday',
+            },
+            {
+              value: '$2,000',
+              description: 'The income out of trading goods on Tuesday',
+            },
+            {
+              value: '$3,000',
+              description: 'The income out of trading goods on Wednesday',
+            },
+            {
+              value: '$4,000',
+              description: 'The income out of trading goods on Thursday',
+              descriptionInfo: 'Sales on Thursday',
+            },
+            {
+              value: '$4,000',
+              description: 'The income out of trading goods on Friday',
+            },
+          ],
+        },
+      },
+      {
+        it: 'Info icons',
+        props: {
+          statistics: [
+            {
+              value: '$500',
+              description: 'Monday',
+              descriptionInfo: 'Sales on Thursday',
+            },
+            {
+              value: '$1,500',
+              descriptionInfo:
+                'I am not visible because there is no description',
+            },
+            {
+              value: '$2,500',
+              description: 'Wednesday',
+              descriptionInfo: 'Sales on Wednesday',
+            },
+            {
+              value: '$2,500',
+              description: 'The income out of trading goods on Thursday',
+              descriptionInfo: 'Sales on Wednesday',
+            },
+            {
+              value: '$4,250,000,000',
+              valueInShort: '$4,3B',
+              description: 'The income out of trading goods on Friday',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    describe: 'trends',
+    its: [
+      {
+        it: 'trends and inverted trends',
+        props: {
+          statistics: [
+            {
+              value: '$500',
+              description: 'Monday',
+              percentage: 21,
+            },
+            {
+              value: '$1,500',
+              description: 'Tuesday',
+              percentage: 21,
+              invertedPercentage: true,
+            },
+            {
+              value: '$2,500',
+              percentage: -11,
+            },
+            {
+              value: '$3,500',
+              description: 'Thursday',
+              percentage: -11,
+              invertedPercentage: true,
+              descriptionInfo: 'Sales on Thursday',
+            },
+            {
+              value: '0',
+              description: 'Friday',
+              percentage: 0,
+              invertedPercentage: true,
+              descriptionInfo: 'Sales on Friday',
             },
           ],
         },
