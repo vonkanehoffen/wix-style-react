949fa973c51926769605e024792901507c492712
Ran Yitzhaki <ranyitz@gmail.com>
2019-08-26 15:18:11 +0300

    add custom stylable webpack plugin

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,29 @@
 const merge = require("lodash/merge");
 const path = require("path");
 const wixStorybookConfig = require("yoshi/config/webpack.config.storybook");
+const { StylableWebpackPlugin } = require("@Stylable/webpack-plugin");
 
 module.exports = (config, env, defaultConfig) => {
   defaultConfig.module.rules[0].use[0].loader = require.resolve("babel-loader");
 
   const newConfig = wixStorybookConfig(defaultConfig);
+
+  // Filter out yoshi's stylable plugin
+  newConfig.plugins = newConfig.plugins.filter(plugin => {
+    return plugin.constructor.name !== 'StylableWebpackPlugin';
+  });
+
+  const stylableWebpackPlugin = new StylableWebpackPlugin({
+    filename: '[name].stylable.bundle.css',
+    optimize: { classNameOptimizations: false, shortNamespaces: false },
+    generate: {
+      runtimeStylesheetId: 'namespace',
+    },
+    legacyRuntime: false,
+    unsafeBuildNamespace: true
+  });
+
+  newConfig.plugins.push(stylableWebpackPlugin);
 
   return merge(newConfig, {
     context: path.resolve(__dirname, "..", "src"),
