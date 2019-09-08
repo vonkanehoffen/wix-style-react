7dde46f716ae163960f3918d89a4bab76890a8b0
Nitay Neeman <nitayneeman@users.noreply.github.com>
2019-09-08 07:47:26 +0300

    <ModalPreviewLayout/> - move docs to "Components" category (#4058)

--- src/ModalPreviewLayout/test/storySettings.js
+++ src/ModalPreviewLayout/test/storySettings.js
@@ -1,7 +1,7 @@
 import { Category } from '../../../stories/storiesHierarchy';
 
 export const storySettings = {
-  category: Category.WIP,
+  category: Category.COMPONENTS,
   storyName: 'ModalPreviewLayout',
   dataHook: 'story-modal-preview-layout',
   testStoryNames: {
