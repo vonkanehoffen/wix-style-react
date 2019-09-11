a96d6cf2ba7cbd8aa0704ab34507260d76783b93
Arijus Šukys <arijuss@wix.com>
2019-09-11 21:40:26 +0300

    Docs/update testkit section (#4075)
    * docs(Testing): add Testing page under Introduction
    
    this is to be linked in each testkit tab, as a general testkits guide
    
    * chore(.storybook/webpack.config.js): update config to support testkit tab features of autodocs
    
    * docs(testkits): add separate doc about testkits for storybook

--- stories/Introduction/index.js
+++ stories/Introduction/index.js
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import Markdown from 'wix-storybook-utils/Markdown';
 
 import Readme from '../../README.md';
+import TestingReadme from '../../docs/usage/testing.md';
 import UsageWithoutYoshiReadme from '../../docs/usage-without-yoshi.md';
 
 import ComponentsCheatsheet from './Cheatsheet/ComponentsCheatsheet';
@@ -13,6 +14,11 @@ storiesOf('Introduction', module)
   .add('Components Cheatsheet', () => <ComponentsCheatsheet />)
   .add('Usage Without Yoshi', () => (
     <Markdown source={UsageWithoutYoshiReadme} />
+  ))
+  .add('Testing', () => (
+    <div style={{ margin: '0 48px', width: '100%', maxWidth: 1161 }}>
+      <Markdown source={TestingReadme} />
+    </div>
   ));
 
 //TODO - add contribution docs links here somehow
