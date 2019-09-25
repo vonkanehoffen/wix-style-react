a342cbab74c9f0cc48ff87a36cd108059ffa3f0e
mosheKerbel <moshek@wix.com>
2019-09-25 14:55:07 +0300

    [Infra] - Zeit storybook deploy (#4149)
    * zeit storybook deploy
    
    * use token and use npx
    
    * dummy change

---
+++ scripts/zeit-deploy.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+console.log('zeit-deploy.js - start (storybook deployment)');
+
+const shell = require('child_process').execSync;
+
+console.log('collecting params');
+
+const token = process.env.ZEIT_TOKEN;
+
+const pr =
+  (process.env.VCS_BRANCH_NAME &&
+    process.env.VCS_BRANCH_NAME.replace(/\D+/g, '')) ||
+  false;
+const domain = pr ? `wix-style-react-pr-${pr}` : 'wix-style-react';
+console.log(`params: token=${token} pr=${pr} domain=${domain}`);
+
+const deploymentCmd = `npx now alias --token=${token} $(npx now ./storybook-static --no-clipboard --token=${token} --public) ${domain}`;
+console.log(`running now deployment: ${deploymentCmd}`);
+const codeResult = shell(deploymentCmd).toString();
+console.log('Done with result:', codeResult);
+
+console.log('zeit-deploy.js - finished (storybook deployment)');
