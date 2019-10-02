da57437c5bf23b122d2cce5b3e1e115f6e11e3ee
Gil Meir <gilme@wix.com>
2019-10-02 14:21:37 +0300

    copied ToggleSwitch.driver.js from backoffice

---
+++ src/ToggleSwitch/ToggleSwitch.driver.js
@@ -0,0 +1,17 @@
+import { toggleSwitchDriverFactory as coreToggleSwitchDriverFactory } from 'wix-ui-core/drivers/vanilla';
+import { StylableDOMUtil } from '@stylable/dom-test-kit';
+import style from './ToggleSwitch.st.css';
+
+export const toggleSwitchDriverFactory = ({ element, eventTrigger }) => {
+  const coreToggleSwitchDriver = coreToggleSwitchDriverFactory({
+    element,
+    eventTrigger,
+  });
+  const stylableDOMUtil = new StylableDOMUtil(style, element);
+
+  return {
+    ...coreToggleSwitchDriver,
+    getSize: () => stylableDOMUtil.getStyleState(element, 'size'),
+    getSkin: () => stylableDOMUtil.getStyleState(element, 'skin'),
+  };
+};
