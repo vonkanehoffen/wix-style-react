ddbe3fac7ea003e519756f466f2f9ea86943116b
Gil Meir <gilme@wix.com>
2019-10-02 14:23:20 +0300

    migrated index.js

--- src/ToggleSwitch/index.js
+++ src/ToggleSwitch/index.js
@@ -1,1 +1,6 @@
-export { default } from './ToggleSwitch.js';
+import ToggleSwitchComponent from './ToggleSwitch';
+import { createHOC } from 'wix-ui-core/dist/src/createHOC';
+
+const ToggleSwitch = createHOC(ToggleSwitchComponent);
+
+export default ToggleSwitch;
