255dc39b3f4b2d0e61464633ec0715da1fecf475
Gil Meir <gilme@wix.com>
2019-10-02 15:25:28 +0300

    adding displayName to the HOCed component

--- src/ToggleSwitch/index.js
+++ src/ToggleSwitch/index.js
@@ -1,6 +1,7 @@
-import ToggleSwitchComponent from './ToggleSwitch';
+import { ToggleSwitch as ToggleSwitchComponent } from './ToggleSwitch';
 import { createHOC } from 'wix-ui-core/dist/src/createHOC';
 
 const ToggleSwitch = createHOC(ToggleSwitchComponent);
+ToggleSwitch.displayName = ToggleSwitchComponent.displayName;
 
 export default ToggleSwitch;
