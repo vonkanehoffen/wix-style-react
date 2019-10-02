962ff364e291584c507480b4dc42512af90380e6
Gil Meir <gilme@wix.com>
2019-10-02 15:26:11 +0300

    migrating unidriver

--- src/ToggleSwitch/test/ToggleSwitch.uni.driver.js
+++ src/ToggleSwitch/test/ToggleSwitch.uni.driver.js
@@ -1,18 +1,24 @@
 import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
+import { StylableDOMUtil } from '@stylable/dom-test-kit';
+import style from './ToggleSwitch.st.css';
 
 export const toggleSwitchDriverFactory = base => {
+  const stylableDOMUtil = new StylableDOMUtil(style, element);
   return {
     ...baseUniDriverFactory(base),
 
-    /** Get the current count */
-    getCountText: async () => base.$('[data-hook="toggleSwitch-count"]').text(),
+    //   /** Get the current count */
+    //   getCountText: async () => base.$('[data-hook="toggleSwitch-count"]').text(),
 
-    /** Click the button */
-    clickButton: async () =>
-      base.$('[data-hook="toggleSwitch-button"]').click(),
+    //   /** Click the button */
+    //   clickButton: async () =>
+    //     base.$('[data-hook="toggleSwitch-button"]').click(),
 
-    /** Get the button's text */
-    getButtonText: async () =>
-      base.$('[data-hook="toggleSwitch-button"]').text(),
+    //   /** Get the button's text */
+    //   getButtonText: async () =>
+    //     base.$('[data-hook="toggleSwitch-button"]').text(),
+    // };
+    getSize: () => stylableDOMUtil.getStyleState(element, 'size'),
+    getSkin: () => stylableDOMUtil.getStyleState(element, 'skin'),
   };
 };
