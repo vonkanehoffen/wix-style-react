6c09535e662c77adb059e1f7ad265300c4225e53
Mykhailo Kotsiievskyi <maxvektor@users.noreply.github.com>
2019-10-03 13:14:55 +0300

    <Barchart /> - add typings (#4189)

---
+++ src/BarChart/index.d.ts
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+export interface BarChartItem {
+  value: number;
+  label?: React.ReactNode;
+  labelShort?: React.ReactNode;
+  description?: React.ReactNode;
+  descriptionInfo?: React.ReactNode;
+}
+
+export interface BarChartProps {
+  items: BarChartItem[];
+  total?: number;
+  dataHook?: string;
+  deprecatedColors?: boolean;
+}
+
+export interface BarChartState {
+  width: number;
+}
+
+export default class BarChart extends React.Component<BarChartProps, BarChartState>  {}
