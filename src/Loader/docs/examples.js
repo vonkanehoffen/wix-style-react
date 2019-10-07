3e6ce853b03e328b7273364403f905ebdfc6029d
mosheKerbel <moshek@wix.com>
2019-10-07 18:21:34 +0300

    improve loader docs (#4217)

---
+++ src/Loader/docs/examples.js
@@ -0,0 +1,16 @@
+export const sizes = `
+<Layout>
+    <Cell span={3}><Loader size="tiny"/></Cell>
+    <Cell span={3}><Loader size="small"/></Cell>
+    <Cell span={3}><Loader size="medium"/></Cell>
+    <Cell span={3}><Loader size="large"/></Cell>
+</Layout>
+`;
+
+export const status = `
+<Layout>
+    <Cell span={3}><Loader status="loading"/></Cell>
+    <Cell span={3}><Loader status="success"/></Cell>
+    <Cell span={3}><Loader status="error"/></Cell>
+</Layout>
+`;
--- src/Loader/docs/index.story.js
+++ src/Loader/docs/index.story.js
@@ -1,6 +1,26 @@
 import { Category } from '../../../stories/storiesHierarchy';
 
 import Loader from '..';
+import {
+  api,
+  code as baseCode,
+  columns,
+  description,
+  divider,
+  header,
+  importExample,
+  playground,
+  tab,
+  tabs,
+  testkit,
+  title,
+} from 'wix-storybook-utils/dist/src/Sections';
+import { baseScope } from '../../../stories/utils/LiveCodeExample';
+import React from 'react';
+import * as examples from './examples';
+
+const code = config =>
+  baseCode({ components: baseScope, compact: true, ...config });
 
 export default {
   category: Category.COMPONENTS,
@@ -15,4 +35,48 @@ export default {
     statusMessage: 'some message here',
     text: '',
   },
+
+  sections: [
+    header({
+      component: (
+        <div style={{ width: '50%' }}>
+          <Loader />
+        </div>
+      ),
+
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/blob/master/src/Loader',
+    }),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          description(`Provides a spinner to be used for async operations.`),
+
+          importExample("import Loader from 'wix-style-react/Loader';"),
+
+          divider(),
+
+          title('Examples'),
+
+          ...[{ title: 'Size', source: examples.sizes }].map(
+            ({ title, source }) =>
+              columns([description({ title }), code({ source })]),
+          ),
+          ...[{ title: 'Status', source: examples.status }].map(
+            ({ title, source }) =>
+              columns([description({ title }), code({ source })]),
+          ),
+        ],
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
+  ],
 };
