01c2850b0bbd6ae54b737501079b175f03be9050
Mykolas Sindeikis <mykolass@wix.com>
2019-10-08 11:16:16 +0300

    <CalendarPanelFooter/> - migrate to .visuals (#4220)

---
+++ src/CalendarPanelFooter/tests/CalendarPanelFooter.visual.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { storySettings } from '../docs/storySettings';
+import CalendarPanelFooter, { defaultDateToStringOptions } from '..';
+
+const tests = [
+  {
+    describe: 'CalendarPanelFooter',
+    its: [
+      {
+        it: 'should render',
+        props: {
+          dataHook: storySettings.dataHook,
+          primaryActionDisabled: false,
+          primaryActionOnClick: () => null,
+          secondaryActionOnClick: () => null,
+          dateToString: date =>
+            date.toLocaleDateString('en-US', defaultDateToStringOptions),
+          selectedDays: {
+            from: new Date('2019-05-27T21:00:00.000Z'),
+            to: new Date('2019-05-27T21:00:00.000Z'),
+          },
+          primaryActionLabel: 'submit',
+          secondaryActionLabel: 'cancel',
+        },
+      },
+    ],
+  },
+];
+
+tests.forEach(({ describe, its }) => {
+  its.forEach(({ it, props }) => {
+    storiesOf(`CalendarPanelFooter/${describe}`, module).add(it, () => (
+      <CalendarPanelFooter {...props} />
+    ));
+  });
+});
