d017b2e986f9cd96f5b05cb70e53ebdd4d291ef4
mosheKerbel <moshek@wix.com>
2019-10-10 15:11:25 +0300

    <PageHeader/> - add examples and extract playground (#4258)

---
+++ src/PageHeader/docs/examples.js
@@ -0,0 +1,15 @@
+export const breadcrumbs = `
+<Page.Header
+  breadcrumbs={<Breadcrumbs activeId="3" items={[{id: '1', value: '#1 item'}, {id: '2', value: '#2 item'}, {id: '3', value: '#3 item'}]} onClick={() => {}}/>}
+  onBackClicked={() => {}}
+  title="Page Header"
+/>
+`;
+
+export const actionBar = `
+<Page.Header
+  actionsBar={<Button>Action</Button>}
+  onBackClicked={() => {}}
+  title="Page Header"
+/>
+`;
--- src/PageHeader/docs/index.story.js
+++ src/PageHeader/docs/index.story.js
@@ -6,6 +6,25 @@ import Button from 'wix-style-react/Button';
 import Breadcrumbs from '../../Page/docs/Breadcrumbs';
 import './PageHeader.scss';
 import { storySettings } from './storySettings';
+import {
+  api,
+  code as baseCode,
+  columns,
+  description,
+  divider,
+  header,
+  importExample,
+  playground,
+  tab,
+  tabs,
+  testkit,
+  title,
+} from 'wix-storybook-utils/dist/src/Sections';
+import { baseScope } from '../../../stories/utils/LiveCodeExample';
+import * as examples from './examples';
+
+const code = config =>
+  baseCode({ components: baseScope, compact: true, ...config });
 
 export default {
   category: storySettings.category,
@@ -34,4 +53,44 @@ export default {
       },
     ],
   },
+
+  sections: [
+    header({
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/blob/master/src/PageHeader',
+    }),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          description(
+            `A header that sticks at the top of the container which minimizes on scroll.`,
+          ),
+
+          importExample("import PageHeader from 'wix-style-react/PageHeader';"),
+
+          divider(),
+
+          title('Examples'),
+
+          ...[{ title: 'Breadcrumbs', source: examples.breadcrumbs }].map(
+            ({ title, source }) =>
+              columns([description({ title }), code({ source })]),
+          ),
+          ...[{ title: 'Actionbar', source: examples.actionBar }].map(
+            ({ title, source }) =>
+              columns([description({ title }), code({ source })]),
+          ),
+        ],
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
+  ],
 };
