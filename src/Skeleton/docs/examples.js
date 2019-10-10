25cdaeb5be87c9faeb62a20fb035dbaa96a438e4
mosheKerbel <moshek@wix.com>
2019-10-10 14:07:41 +0300

    <Skeleton/> - Add examples and playground to story (#4256)

---
+++ src/Skeleton/docs/examples.js
@@ -0,0 +1,22 @@
+export const sizes = `
+<Skeleton
+  content={[
+    {
+      size: 'small',
+      type: 'line'
+    },
+    {
+      size: 'large',
+      type: 'line'
+    },
+    {
+      size: 'medium',
+      type: 'line'
+    },
+    {
+      size: 'full',
+      type: 'line'
+    }    
+  ]}
+ />
+`;
--- src/Skeleton/docs/index.story.js
+++ src/Skeleton/docs/index.story.js
@@ -1,5 +1,25 @@
 import Skeleton from '..';
 import { Category } from '../../../stories/storiesHierarchy';
+import {
+  api,
+  code as baseCode,
+  columns,
+  description,
+  divider,
+  header,
+  importExample,
+  playground,
+  tab,
+  tabs,
+  testkit,
+  title,
+} from 'wix-storybook-utils/dist/src/Sections';
+import { baseScope } from '../../../stories/utils/LiveCodeExample';
+import * as examples from './examples';
+import React from 'react';
+
+const code = config =>
+  baseCode({ components: baseScope, compact: true, ...config });
 
 const exampleContent = [
   {
@@ -52,4 +72,46 @@ export default {
   exampleProps: {
     content: exampleContent,
   },
+
+  sections: [
+    header({
+      component: (
+        <div style={{ width: '50%' }}>
+          <Skeleton />
+        </div>
+      ),
+
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/blob/master/src/Skeleton',
+    }),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          description(
+            `Placeholder for filling up screen, usually for when some async operation is ongoing.`,
+          ),
+
+          importExample("import Skeleton from 'wix-style-react/Skeleton';"),
+
+          divider(),
+
+          title('Examples'),
+
+          ...[{ title: 'Sizes', source: examples.sizes }].map(
+            ({ title, source }) =>
+              columns([description({ title }), code({ source })]),
+          ),
+        ],
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
+  ],
 };
