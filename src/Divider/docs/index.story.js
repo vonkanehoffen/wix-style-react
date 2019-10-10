b8eacf363c283d0d25fa0be1648e1e45aca0215d
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-10 11:52:57 +0300

    docs(<Divider/>) - improve example title

--- src/Divider/docs/index.story.js
+++ src/Divider/docs/index.story.js
@@ -64,7 +64,7 @@ export default {
 
           columns([
             description({
-              title: 'Plain Example',
+              title: 'Horizontal Example',
               text: 'By default, the divider is horizontal',
             }),
 
