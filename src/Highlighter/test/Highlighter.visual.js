bbafa9f74a17aea9a04128f28e6300fa0a30637b
mykas <mykolass@wix.com>
2019-10-14 12:09:15 +0300

    <Highligher/> - migrate to visuals

---
+++ src/Highlighter/test/Highlighter.visual.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Highlighter from '../Highlighter';
+
+const defaultProps = {
+  children: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+  consequuntur earum eius eum fugiat`,
+};
+
+const tests = [
+  {
+    describe: 'Basic',
+    its: [
+      {
+        it: 'should render',
+      },
+    ],
+  },
+];
+
+tests.forEach(({ describe, its }) => {
+  its.forEach(({ it }) => {
+    storiesOf(`Highlighter/${describe}`, module).add(it, () => (
+      <div>
+        <Highlighter {...defaultProps} />
+      </div>
+    ));
+  });
+});
