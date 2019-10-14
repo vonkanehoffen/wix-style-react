1d4c015e8e31f9d183927af5713ce128688ae0ef
Mykolas Sindeikis <mykolass@wix.com>
2019-10-14 14:11:54 +0300

    <Heading/> - migrate to visuals (#4282)

---
+++ src/Heading/test/Heading.visual.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Heading, { APPEARANCES } from '..';
+
+const defaultProps = {
+  light: false,
+  appearance: 'H1',
+  children: 'Hey there, good looking',
+};
+
+const tests = [
+  {
+    describe: 'basic',
+    its: [
+      ...Object.values(APPEARANCES).map(appearance => ({
+        it: `appearance prop ${appearance}`,
+        props: {
+          appearance,
+        },
+      })),
+      {
+        it: 'light prop - dark',
+      },
+      {
+        it: 'light prop - light',
+        props: {
+          light: true,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'ellipsis',
+    its: [
+      {
+        it: 'with',
+        props: {
+          ellipsis: true,
+          children: 'very very long text',
+        },
+        container: {
+          style: { width: '267px' },
+        },
+      },
+    ],
+  },
+];
+
+tests.forEach(({ describe, its }) => {
+  its.forEach(({ it, props, container }) => {
+    storiesOf(`Heading/${describe}`, module).add(it, () => (
+      <div {...container}>
+        <Heading {...defaultProps} {...props} />
+      </div>
+    ));
+  });
+});
