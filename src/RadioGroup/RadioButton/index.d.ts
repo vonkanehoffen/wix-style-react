4519ab79d88a4fc01df61a3603d97920e17f8bf2
Anastasiia Povzyk <anastasiiap@wix.com>
2019-10-17 13:58:59 +0300

    Add RadioButton props type

---
+++ src/RadioGroup/RadioButton/index.d.ts
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {WixComponentProps} from "../../BaseComponents/WixComponent";
+
+export interface RadioButtonProps extends WixComponentProps {
+  value?: string | number,
+  vAlign?: RadioButtonVerticalAlign,
+  name?: string,
+  onChange?: Function,
+  checked?: boolean,
+  disabled?: boolean,
+  children?: any,
+  style?: object,
+  type?: RadioButtonType,
+  lineHeight?: string,
+  selectionArea?: RadioButtonSelectionArea,
+  content?: React.ReactNode,
+}
+
+export type RadioButtonVerticalAlign = 'center' | 'top';
+export type RadioButtonType = 'default' | 'button';
+export type RadioButtonSelectionArea = 'none' | 'hover' | 'always';
+
+export default class RadioButton extends React.PureComponent<RadioButtonProps> {
+  static Radio: RadioButton
+}
