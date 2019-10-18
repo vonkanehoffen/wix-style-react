5b68a446c45c4106c19f2851c2e34697d9da453f
Anastasiia Povzyk <anastasiiap@wix.com>
2019-10-18 13:23:46 +0300

    Change exports

--- src/Notification/index.d.ts
+++ src/Notification/index.d.ts
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {WixComponentProps} from "../BaseComponents/WixComponent";
-import {GLOBAL_NOTIFICATION, LOCAL_NOTIFICATION, STICKY_NOTIFICATION} from './Notification';
 import TextLabel from './TextLabel';
 import CloseButton from '../CloseButton';
 import ActionButton from './ActionButton';
@@ -16,11 +15,10 @@ export interface NotificationProps extends WixComponentProps {
 }
 
 export type NotificationTheme = 'standard' | 'error' | 'success' | 'warning' | 'premium';
-export type NotificationType = GLOBAL_NOTIFICATION | LOCAL_NOTIFICATION | STICKY_NOTIFICATION;
+export type NotificationType = 'local' | 'global' | 'sticky';
 
 export default class Notification extends React.PureComponent<NotificationProps> {
   static CloseButton: typeof CloseButton;
   static TextLabel: typeof TextLabel;
   static ActionButton: typeof ActionButton;
 }
-
