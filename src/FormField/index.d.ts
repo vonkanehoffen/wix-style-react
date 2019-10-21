76e41d1f5eff8cda598f56eac1204fcfc6e3f436
Anastasiia Povzyk <anastasiiap@wix.com>
2019-10-21 17:52:34 +0300

    Add functional child

--- src/FormField/index.d.ts
+++ src/FormField/index.d.ts
@@ -1,21 +1,28 @@
 import * as React from 'react';
-import {TooltipProps} from "../Tooltip";
+import { TooltipProps } from '../Tooltip';
+
+export type FormFieldFunctionalChildrenArg = {
+  setCharactersLeft: (left: number) => void;
+};
+
+export type FormFieldFunctionalChildren = (
+  arg: FormFieldFunctionalChildrenArg,
+) => React.ReactNode;
 
 export interface FormFieldProps {
-  children?: React.ReactNode | Function,
-  stretchContent?: boolean,
-  label?: React.ReactNode,
-  labelSize?: FormFieldLabelSize,
-  labelPlacement?: FormFieldPlacement,
-  required?: boolean,
-  infoContent?: React.ReactNode,
-  infoTooltipProps?: TooltipProps,
-  id?: string,
-  dataHook?: string,
+  children?: React.ReactNode | FormFieldFunctionalChildren;
+  stretchContent?: boolean;
+  label?: React.ReactNode;
+  labelSize?: FormFieldLabelSize;
+  labelPlacement?: FormFieldPlacement;
+  required?: boolean;
+  infoContent?: React.ReactNode;
+  infoTooltipProps?: TooltipProps;
+  id?: string;
+  dataHook?: string;
 }
 
 export type FormFieldLabelSize = 'small' | 'medium';
 export type FormFieldPlacement = 'top' | 'right' | 'left';
 
-export default class FormField extends React.PureComponent<FormFieldProps> {
-}
+export default class FormField extends React.PureComponent<FormFieldProps> {}
