4421548133fd85f5ac6d8c172e28cae4642f8b5a
Simas Toleikis <simast@wix.com>
2019-10-22 14:44:20 +0300

    Add visual tests for Card.Subheader component. (#4349)

---
+++ src/Card/Subheader/test/Subheader.visual.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Subheader from '..';
+import Button from '../../../Button';
+
+const suffix = (
+  <Button onClick={() => undefined} size="small" theme="fullblue">
+    Button
+  </Button>
+);
+
+const tests = [
+  {
+    describe: 'Subheader',
+    its: [
+      {
+        it: 'Basic',
+        props: {
+          title: 'Subheader title',
+          suffix,
+        },
+      },
+      {
+        it: 'Neutral skin',
+        props: {
+          title: 'Subheader title',
+          skin: 'neutral',
+          suffix,
+        },
+      },
+      {
+        it: 'Title ellipsis',
+        props: {
+          title: 'Title '.repeat(100),
+          suffix,
+        },
+      },
+    ],
+  },
+];
+
+tests.forEach(({ describe, its }) => {
+  its.forEach(({ it, props }) => {
+    storiesOf(`Card/${describe}`, module).add(it, () => (
+      <Subheader {...props} />
+    ));
+  });
+});
