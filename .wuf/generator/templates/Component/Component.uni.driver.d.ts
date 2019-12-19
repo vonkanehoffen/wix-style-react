dc1be3aa53f2940c1e6e3d8d2ac6426b0b89d02c
Tal Zaidman <talza@wix.com>
2019-12-19 14:32:57 +0200

    Generator- adding "d.ts" file to component unidriver (#4724)

---
+++ .wuf/generator/templates/Component/Component.uni.driver.d.ts
@@ -0,0 +1,7 @@
+import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
+
+export interface {%ComponentName%}Driver extends BaseUniDriver {
+  getCountText(): Promise<string>;
+  clickButton(): Promise<void>;
+  getButtonText(): Promise<string>;
+}
