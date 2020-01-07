2b60a825e9b187b5a627f14fc63d3cf318844158
Simas Toleikis <simast@wix.com>
2020-01-07 13:29:17 +0200

    Align SocialButton text to center (based on specs). (#4808)

--- src/SocialButton/test/SocialButton.visual.js
+++ src/SocialButton/test/SocialButton.visual.js
@@ -15,14 +15,19 @@ const tests = [
   },
   {
     describe: 'text',
-    its: icons.map(icon => ({ it: icon, props: { icon, text: 'Share me' } })),
+    its: icons.map(icon => ({
+      it: icon,
+      props: { icon, text: 'Share me please!' },
+    })),
   },
 ];
 
 tests.forEach(({ describe, its }) => {
   its.forEach(({ it, props }) => {
     storiesOf(`SocialButton/${describe}`, module).add(it, () => (
-      <SocialButton {...props} />
+      <div style={{ maxWidth: 100 }}>
+        <SocialButton {...props} />
+      </div>
     ));
   });
 });
