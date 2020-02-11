d7a26fd470bf5ddc9e7eaeb253926466d3927e4d
Egozi <egozieran@gmail.com>
2020-02-11 13:08:44 +0200

    Revert body styles, fix types (#4975)

--- src/FontUpgrade/FontUpgrade.js
+++ src/FontUpgrade/FontUpgrade.js
@@ -1,35 +1,29 @@
 import React from 'react';
+import styles from './FontUpgrade.scss';
 import PropTypes from 'prop-types';
 import { FontUpgradeContext } from './context';
 
-const styles = {
-  '--wsr-font-family': 'var(--wix-font-stack)',
-  '--wsr-font-weight-thin': 'var(--wix-font-weight-thin)',
-  '--wsr-font-weight-normal': 'var(--wix-font-weight-normal)',
-  '--wsr-font-weight-bold': 'var(--wix-font-weight-bold)',
-};
-
 const FontUpgrade = ({ dataHook, active = true, children }) => {
-  if (active) {
-    Object.entries(styles).forEach(([key, value]) =>
-      document.body.style.setProperty(key, value),
-    );
-  } else {
-    Object.keys(styles).map(key => document.body.style.removeProperty(key));
-  }
   return (
-    <FontUpgradeContext.Provider value={{ active }}>
-      <span data-hook={dataHook} children={children} />
+    <FontUpgradeContext.Provider value={{ active, styles }}>
+      <span
+        data-hook={dataHook}
+        className={active ? styles.root : null}
+        children={children}
+      />
     </FontUpgradeContext.Provider>
   );
 };
 
-FontUpgrade.Proptypes = {
+FontUpgrade.propTypes = {
   /** Applied as data-hook HTML attribute that can be used to create driver in testing */
   dataHook: PropTypes.string,
 
   /** Sets the Madefor font upgrade active when true (which is by default) */
   active: PropTypes.bool,
+
+  /** A renderable node */
+  children: PropTypes.node,
 };
 
 export default FontUpgrade;
--- src/FontUpgrade/index.d.ts
+++ src/FontUpgrade/index.d.ts
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
 export interface FontUpgradeProps {
+  dataHook?: string;
   active?: boolean;
+  children?: React.ReactNode;
 }
 
 export default class FontUpgrade extends React.PureComponent<
