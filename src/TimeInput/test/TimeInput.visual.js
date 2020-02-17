872cfc87bc013f6e88a94e243ceee41cc177b258
Egozi <egozieran@gmail.com>
2020-02-17 16:37:29 +0200

    <TimeInput/> - fix visuals (#5010)

--- src/TimeInput/test/TimeInput.visual.js
+++ src/TimeInput/test/TimeInput.visual.js
@@ -1,44 +1,33 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import moment from 'moment';
 
 import TimeInput from '..';
+import { snap, visualize } from 'storybook-snapper';
 
 const value1 = moment(0).set({ hour: 9, minute: 45 });
 const value2 = moment(0).set({ hour: 16, minute: 21 });
 
-const tests = [
-  {
-    describe: 'TimeInput',
-    its: [
-      {
-        it: 'different variations',
-        story: () => (
-          <>
-            <TimeInput defaultValue={value1} />
-            <br />
-            <TimeInput defaultValue={value1} disabled />
-            <br />
-            <TimeInput defaultValue={value1} disabled dashesWhenDisabled />
-            <br />
-            <TimeInput defaultValue={value1} disableAmPm />
-            <br />
-            <TimeInput defaultValue={value2} rtl />
-            <br />
-            <TimeInput defaultValue={value2} rtl disabled />
-            <br />
-            <TimeInput defaultValue={value2} rtl disabled dashesWhenDisabled />
-            <br />
-            <TimeInput defaultValue={value2} rtl disableAmPm />
-          </>
-        ),
-      },
-    ],
-  },
-];
-
-tests.forEach(({ describe, its }) => {
-  its.forEach(({ it, story }) => {
-    storiesOf(describe, module).add(it, story);
+visualize('TimeInput', () => {
+  snap('different variations', done => {
+    document.fonts.onloadingdone = done;
+    return (
+      <>
+        <TimeInput defaultValue={value1} />
+        <br />
+        <TimeInput defaultValue={value1} disabled />
+        <br />
+        <TimeInput defaultValue={value1} disabled dashesWhenDisabled />
+        <br />
+        <TimeInput defaultValue={value1} disableAmPm />
+        <br />
+        <TimeInput defaultValue={value2} rtl />
+        <br />
+        <TimeInput defaultValue={value2} rtl disabled />
+        <br />
+        <TimeInput defaultValue={value2} rtl disabled dashesWhenDisabled />
+        <br />
+        <TimeInput defaultValue={value2} rtl disableAmPm />
+      </>
+    );
   });
 });
